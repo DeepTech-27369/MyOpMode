47bc695fe2b6d4b6cfab4a7bea258ebd9665baa3
FireblazingDragon <84352481+FireblazingDragon@users.noreply.github.com>
2026-01-06 19:24:18 -0500

    Refactor MecanumFieldOrientatedOpMode and add drive methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedOpMode.java
@@ -1,47 +1,65 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.mechanisms;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
-import org.firstinspires.ftc.teamcode.mechanisms.Intake;
-//import org.firstinspires.ftc.teamcode.mechanisms.Shooter;
-//ee
-@TeleOp(name = "Mecanum No Shooter", group = "TeleOp")
-public class MecanumFieldOrientatedOpMode extends OpMode {
-    MecanumDrive drive = new MecanumDrive();
-    double forward, strafe, rotate;
-    Intake intake = new Intake();
-    //Shooter shooter = new Shooter();
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-    @Override
-    public void init() {
-        drive.init(hardwareMap);
-        intake.init(hardwareMap);
-        //shooter.init(hardwareMap);
+public class MecanumDrive {
+    private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+    private IMU imu;
+    public void init(HardwareMap hwMap) {
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu = hwMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot. LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
     }
 
-    @Override
-    public void loop() {
-        // Drive
-        forward = gamepad1.left_stick_y;
-        strafe = gamepad1.left_stick_x;
-        rotate = gamepad1.right_stick_x;
+    public void drive(double forward, double strafe, double rotate) {
+        double frontLeftPower = forward + strafe + rotate;
+        double backLeftPower = - forward - strafe + rotate;
+        double frontRightPower = - forward + strafe + rotate;
+        double backRightPower = forward - strafe + rotate;
 
-        drive.driveFieldRelative(forward, strafe, rotate);
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
 
-        // Intake
-        if (gamepad2.left_bumper) {
-            intake.run();
-        } else {
-            intake.stop();
-        }
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
 
-        // Shooter
-        /* if (gamepad2.right_bumper) {
-            shooter.shoot();
-        } else {
-            shooter.stop();
-        } */
+        frontLeftMotor.setPower(maxSpeed * (frontLeftPower / maxPower));
+        backLeftMotor.setPower(maxSpeed * (backLeftPower / maxPower));
+        frontRightMotor.setPower(maxSpeed * (frontRightPower / maxPower));
+        backRightMotor.setPower(maxSpeed * (backRightPower / maxPower));
+    }
+
+    public void driveFieldRelative(double forward, double strafe, double rotate) {
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        this.drive(newForward, newStrafe, rotate);
     }
 }
