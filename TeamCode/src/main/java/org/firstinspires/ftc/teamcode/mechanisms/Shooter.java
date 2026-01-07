289e12378e73703b9a150e485398eb30d14c4f3f
vivan-j <vivan.jaiswal924@gmail.com>
2026-01-07 16:15:42 -0500

    shooter stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Shooter.java
@@ -1,4 +1,4 @@
-/* package org.firstinspires.ftc.teamcode.mechanisms;
+package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -11,6 +11,9 @@ public class Shooter {
     public void init(HardwareMap hwMap) {
         leftShooterMotor = hwMap.get(DcMotor.class, "leftShooter");
         rightShooterMotor = hwMap.get(DcMotor.class, "rightShooter");
+
+        leftShooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightShooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         stop();
     }
@@ -25,4 +28,4 @@ public class Shooter {
         rightShooterMotor.setPower(0.0);
     }
 
-} */
+}
